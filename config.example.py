4edc9e58400874aff987e1234f88bf891fb712cb
Ahmed Seref GUNEYSU <serefguneysu@gmail.com>
2015-02-11 23:20:25 +0200

    Sytax error on config fixed

--- config.example.py
+++ config.example.py
@@ -2,7 +2,7 @@
     Document this
 """
 config_local = dict(
-    url='http://localhost/rest_server_end_point'
+    url='http://localhost/rest_server_end_point',
     services_token='1c33c818dc4142ba1c2f18f6716fcb1efe135181',
     domain='localhost', username='', key='')
 
